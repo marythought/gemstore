3e2038eaacecc5a2a17e6043c4e7e8c79dfef6df
Mary Dickson Diaz <mdickson@gmail.com>
2015-10-12 15:54:49 -0700

    finish chapter 4, add directives and html renders

--- public/app.js
+++ public/app.js
@@ -5,31 +5,51 @@
     this.products = gems;
   });
 
-  app.controller('TabController', function(){
-    this.tab = 1;
-
-    this.setTab = function(setTab) {
-      this.tab = setTab;
-    };
-    this.isSet = function(checkTab){
-      return this.tab === checkTab;
-    };
-  });
-
-  app.controller('GalleryController', function(){
-    this.current = 0;
-
-    this.setCurrent = function(index){
-      this.current = index;
-    };
-  });
-
   app.controller("ReviewController", function(){
     this.review = {};
     this.addReview = function(product) {
       this.review.createdOn = Date.now();
       product.reviews.push(this.review);
       this.review = {};
+    };
+  });
+
+  app.directive("productGallery", function() {
+    return {
+      restrict:"E",
+      templateUrl: "product-gallery.html",
+      controller: function(){
+        this.current = 0;
+        this.setCurrent = function(index){
+          this.current = index || 0;
+        };
+      },
+      controllerAs: "gallery"
+    };
+  });
+
+  app.directive('productTabs', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-tabs.html',
+      controller: function(){
+        this.tab = 1;
+
+        this.setTab = function(setTab) {
+          this.tab = setTab;
+        };
+        this.isSet = function(checkTab){
+          return this.tab === checkTab;
+        };
+      },
+      controllerAs: "tab"
+    };
+  });
+
+  app.directive('productTitle', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-title.html'
     };
   });
 
